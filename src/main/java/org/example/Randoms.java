072ff3b9006a9f8b9064439df48ca0c70e880caa
Yuriy Sviridov <crip.shooter@gmail.com>
2022-10-25 10:41:45 +0300

    add Main & Randoms

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+
+    private final int diff;
+    private int itNumber;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        diff = max - min;
+        random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new AdditionalIterator();
+    }
+
+    private class AdditionalIterator implements Iterator<Integer> {
+
+        @Override
+        public boolean hasNext() {
+            itNumber = random.nextInt(diff + 1);
+            return itNumber < max;
+        }
+
+        @Override
+        public Integer next() {
+            return itNumber += min;
+        }
+    }
+}
